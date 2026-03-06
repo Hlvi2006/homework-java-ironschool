cb7dff2cf2b46c45d4b60d060fde9ee0f6f83fa8
Ulvi Haciyev <ulvihaciyev551@gmail.com>
2026-03-06 14:58:35 +0400

    Some extrases added

---
+++ demo/src/main/java/com/example/demo/models/Student.java
@@ -0,0 +1,9 @@
+package com.example.demo.models;
+
+public class Student {
+    private String studentId;
+    private String name;
+    private String address;
+    private String email;
+    
+}
--- demo/src/main/java/com/example/demo/models/Teacher.java
+++ demo/src/main/java/com/example/demo/models/Teacher.java
@@ -1,7 +1,31 @@
 package com.example.demo.models;
 
+import java.util.UUID;
+
 public class Teacher {
     private String teacherId;
     private String name;
     private double salary;
+
+    public Teacher(double salary, String name) {
+        this.salary = salary;
+        this.name = name;
+        this.teacherId= UUID.randomUUID().toString();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 }
